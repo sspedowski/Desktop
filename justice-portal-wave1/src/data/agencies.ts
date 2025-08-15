cd3e38ad2b6441eda47c7eae2bf3a02777870d33
sspedowski <sspedowski2023@gmail.com>
2025-08-14 22:08:58 -0400

    commit

--- justice-portal-wave1/src/data/agencies.ts
+++ justice-portal-wave1/src/data/agencies.ts
@@ -10,6 +10,7 @@ export const agencies: Agency[] = [
   { id: "agency-1", name: "Oversight Agency A", code: "OVR-A", passphrase: "OVR-A-2025" },
   { id: "agency-2", name: "Media Contact", code: "MEDIA-1", passphrase: "MEDIA-1-2025" },
   { id: "agency-3", name: "Internal Reviewer", code: "INTERNAL", passphrase: "INTERNAL-2025" },
+  { id: "agency-4", name: "Seeking Justice", code: "SEEKINGJUSTICE", passphrase: "Thankyouforcaring2025!" },
 ];
 
 export function findAgencyByCode(code: string) {
